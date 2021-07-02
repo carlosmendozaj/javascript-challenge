05a12ca91e97d568733be4db7bc6c45c81b6a62c
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-07-01 22:23:06 -0500

    Update app.js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// SELECT tbody
+var tbody = d3.select("tbody");
+
+
+tableData.forEach((ufoSightings) => {
+    row = tbody.append("tr");
+    Object.values(ufoSightings).forEach(value => row.append("td").text(value));
+  });
+
+  
+// Filters:
+
