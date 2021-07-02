18d1ce43173520cd10a5e84853238c89ce24d544
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-07-01 22:09:04 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,22 +1,23 @@
 // from data.js
 var tableData = data;
 
-// Select the button
+// YOUR CODE HERE!
+
+// Select tbody
+var tbody = d3.select("tbody");
+
+// button
 var button = d3.select("#filter-btn");
 
-// Select the form
 var form = d3.select("#datetime");
 
-// Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-var tbody = d3.select("tbody");
-// tbody.html("");
 
-tableData.forEach((ufoSighting) => {
+tableData.forEach((ufoSightings) => {
   row = tbody.append("tr");
-  Object.values(ufoSighting).forEach(value => row.append("td").text(value));
+  Object.values(ufoSightings).forEach(value => row.append("td").text(value));
 });
 
 // Complete the event handler function for the form
